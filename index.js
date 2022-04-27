228ca3caf7ff8a48c44150d8cb381a266e58220d
Samuel Banya <sbanya@fastmail.com>
2022-04-27 18:42:37 -0400

    Adding latest changes as per assignment

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // code your solution here
+function saturdayFun(activity) {
+    if (activity) {
+        return "This Saturday, I want to " + activity + "!";
+    }
+
+    else {
+        let activity = "roller-skate";
+        return "This Saturday, I want to " + activity + "!";
+    }
+}
+
+function mondayWork(activity) {
+    if (activity) {
+        return "This Monday, I will " + activity + ".";
+    }
+
+    else {
+        let activity = "go to the office";
+        return "This Monday, I will " + activity + ".";        
+    }
+}
+
+
+function wrapAdjective(flair = "*") {
+    return function (adjective = "special") {
+        return "You are " + `${flair}` + `${adjective}` + `${flair}` + "!";
+    }
+}
+
+wrapAdjective("%")("a dedicated programmer");
+
+
